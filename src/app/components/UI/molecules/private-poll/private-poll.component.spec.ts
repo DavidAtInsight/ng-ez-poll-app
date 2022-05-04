5a8dab0e8db5fbbe0d3a8785a17aa294a2e18d70
DavidAtInsight <103692485+DavidAtInsight@users.noreply.github.com>
2022-05-04 17:11:09 -0400

    Increase test coverage
    Add Mock Service and Spy Method

--- src/app/components/UI/molecules/private-poll/private-poll.component.spec.ts
+++ src/app/components/UI/molecules/private-poll/private-poll.component.spec.ts
@@ -1,4 +1,8 @@
+import { TemplateRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockProvider, ngMocks } from 'ng-mocks';
+
+import { NzNotificationService } from 'ng-zorro-antd/notification';
 
 import { PrivatePollComponent } from './private-poll.component';
 
@@ -8,7 +12,8 @@ describe('PrivatePollComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-        declarations: [ PrivatePollComponent ]
+        declarations: [ PrivatePollComponent ],
+        providers: [ MockProvider(NzNotificationService) ]
         })
         .compileComponents();
     });
@@ -22,4 +27,13 @@ describe('PrivatePollComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should call onSubmit with templateRef', () => {
+        const onSubmitSpy = jest.spyOn(component, 'onSubmit');
+        const templateRef = ngMocks.findTemplateRef('notificationTemplate');
+
+        component.onSubmit(templateRef);
+
+        expect(onSubmitSpy).toBeCalledWith(templateRef);
+    })
 });
--- src/app/components/UI/organisms/create-poll/create-poll.component.spec.ts
+++ src/app/components/UI/organisms/create-poll/create-poll.component.spec.ts
@@ -22,4 +22,6 @@ describe('CreatePollComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    
 });
